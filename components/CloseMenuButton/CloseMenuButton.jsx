a0dea048fdcfe824cf29b215e9ec299133bd38a6
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-08-17 16:42:40 +0300

    removed icon imports in closemenubutton comp

--- components/CloseMenuButton/CloseMenuButton.jsx
+++ components/CloseMenuButton/CloseMenuButton.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmarkCircle } from '@fortawesome/free-regular-svg-icons';
+
 
 const CloseMenuButton = ({closeFunction}) => {
   return (
